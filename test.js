9a0c3c32783e8b74dbaf0700bb133d1b1fd7f1e8
Bogdan <lebed4086@gmail.com>
2021-09-30 01:46:49 +0300

    res

---
+++ test.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    meat: 1,
+    vegetables: 1,
+    cheese: 1,
+    meat_price: 100,
+    vegetables_price: 50,
+    cheese_price: 80,
+    meat_mass: 150,
+    vegetables_mass: 150,
+    cheese_mass: 150,
+    price: 0
+  }
+})
